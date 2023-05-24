3681d924f41a6cdec22c730278f67cb0d2f820d9
Rama <ramiroquesadaperez@gmail.com>
2023-05-24 04:14:37 -0300

    getRanking devuelve 2 arrays con los mejores 15 ordenados

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -2,14 +2,46 @@ const { response } = require('express');
 
 const Record = require('../models/record');
 
+// const getRanking = async (req, res = response) => {
+// 	try {
+// 		const records = await Record.find();
+
+// 		res.status(200).json({
+// 			ok: true,
+// 			msg: 'Ranking',
+// 			records,
+// 		});
+// 	} catch (error) {
+// 		res.status(500).json({
+// 			ok: false,
+// 			msg: 'Hable con el administrador',
+// 		});
+// 	}
+// };
+
 const getRanking = async (req, res = response) => {
 	try {
-		const records = await Record.find();
+		
+		const limit = 15;
+
+		// Obtener los mejores tiempos y menor cantidad de clicks de gamemode id 2
+		const bestTimesQuery = await Record.find({ 'gameMode.modeId': 2 })
+			.sort({ 'time.minutes': 1, 'time.seconds': 1, 'time.miliseconds': 1, 'clicks': 1 })
+			.limit(limit);
+
+		// Obtener la mayor cantidad de clicks en menos tiempo de gamemode id 1
+		const mostClicksQuery = await Record.find({ 'gameMode.modeId': 1 })
+			.sort({ 'clicks': -1, 'time.minutes': 1, 'time.seconds': 1, 'time.miliseconds': 1 })
+			.limit(limit);
+
+		// Ejecutar ambas consultas de forma concurrente
+		const [memoryRecords, clickAllRecords] = await Promise.all([bestTimesQuery, mostClicksQuery]);
 
 		res.status(200).json({
 			ok: true,
 			msg: 'Ranking',
-			records,
+			memoryRecords,
+			clickAllRecords,
 		});
 	} catch (error) {
 		res.status(500).json({
@@ -17,6 +49,7 @@ const getRanking = async (req, res = response) => {
 			msg: 'Hable con el administrador',
 		});
 	}
+
 };
 
 const postRecord = async (req, res = response) => {
